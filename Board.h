b60cffbaa35fa1947bb68f8e43474115a890c9a2
Yonatanr6 <33617974+Yonatanr6@users.noreply.github.com>
2018-05-09 14:29:36 +0300

    Add files via upload

--- Board.h
+++ Board.h
@@ -111,11 +111,11 @@ public:
 	}
     };
     
-//    ~Board(){
-//        for(int i = 0; i < size; i++)
-//            delete[] board[i];
-//        delete[] board;
-//    }
+    ~Board(){
+        for(int i = 0; i < size; i++)
+            delete[] board[i];
+        delete[] board;
+    }
     
 };
 
